e743b0772e33e7717576215105c261cfe9611a1f
Deniz Gökçin <dgokcin@gmail.com>
2020-07-24 09:13:43 +0300

    finished get_user_profile test

--- tests/test_users_api.py
+++ tests/test_users_api.py
@@ -1,13 +1,28 @@
+import unittest
 from api.users_api import *
 
 
-def test_create_user():
-    with db.atomic() as transaction:
-        display_name = "temp_user"
-        country = "tr"
+class TestUserMethods(unittest.TestCase):
 
-        success = create_user(display_name, country)
-        # transaction.rollback()
+    def test_create_user_profile(self):
+        with db.atomic() as transaction:
+            display_name = "temp_user"
+            country = "tr"
 
-    assert success is True
+            success = create_user_profile(display_name, country)
+            transaction.rollback()
 
+        self.assertEqual(success, True)
+
+    def test_get_user_profile(self):
+        guid = "5b69053b-8a51-42c1-b6cf-4473c4c0bede"
+        user = get_user_profile(guid)
+        expected = {'user_id': '5b69053b-8a51-42c1-b6cf-4473c4c0bede',
+                    'display_name': 'temp_user',
+                    'points': 0.0, 'rank': 2147483647}
+
+        self.assertEqual(user, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
