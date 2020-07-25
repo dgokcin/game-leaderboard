e35ebbdbed4d604fbf45b2ac777ec8f4289e88d1
Deniz Gökçin <dgokcin@gmail.com>
2020-07-25 22:50:48 +0300

    brought back tests

--- flask/app/tests/test_module.py
+++ flask/app/tests/test_module.py
@@ -1,13 +1,35 @@
 import unittest
-from app import app
-from mockupdb import MockupDB, go, Command
-from pymongo import MongoClient
-from json import dumps
+from mongoengine.connection import disconnect
+from mongoengine import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        pass
+
+
+class User(Document):
+    user_id = UUIDField(primary_key=True)
+    points = FloatField()
+    rank = IntField()
+    country = StringField()
+    display_name = StringField(unique=True)
+
+
+class TestPerson(unittest.TestCase):
+
+    def setUp(self):
+        connect('mongoenginetest', host='mongomock://localhost')
+
+    def tearDown(self):
+        disconnect('mongoenginetest')
+
+    def test_create_user(self):
+        User(user_id='1376a111fba449bb800b3f0bca64b3a3',
+                    display_name='deniz',
+                    points=1000,
+                    rank=1,
+                    country='tr').save()
+
+        usr = User.objects().first()
+        self.assertEqual(usr.display_name, 'deniz')
 
 
 if __name__ == '__main__':
